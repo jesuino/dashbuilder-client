694fc179479bc960ea410dcf2175651cb04fbfa1
William Siqueira <william.fatecsjc@gmail.com>
2022-01-11 11:48:31 -0300

    Supporting external YML and hide editor

--- index.js
+++ index.js
@@ -37,14 +37,18 @@ layoutTemplates:
                       enabled: 'true'
                   dataSetLookup:
                     dataSetUuid: pop`;
-var currentContent = "";
+var currentValue = "";
+var hideEditor = false;
 const editorEl = document.getElementById("editor");
+const updateBtn = document.getElementById("updateBtn");
+const hideEditorBtn = document.getElementById("hideEditorBtn");
+const editorContainer = document.getElementById("editorContainer");
+const dbContainer = document.getElementById("dbContainer");
 const editor = ace.edit("editor");
 editor.session.setMode("ace/mode/yaml");
-editor.session.setValue(defaultYML);
 editor.setOptions({
-    fontSize: "12pt"
-  });
+  fontSize: "12pt",
+});
 
 const toJson = (value) => {
   var json = jsyaml.load(value);
@@ -53,9 +57,11 @@ const toJson = (value) => {
 
 const send = () => {
   var value = editor.getValue();
-  currentContent = value;
-  value = toJson(value);
-  document.getElementById("dbFrame").contentWindow.postMessage(value, null);
+  currentValue = value;
+  if (value.trim() !== "") {
+    value = toJson(value);
+    document.getElementById("dbFrame").contentWindow.postMessage(value, null);
+  }
 };
 
 const onkey = (e) => {
@@ -64,16 +70,47 @@ const onkey = (e) => {
   }
 };
 
+const hideShowEditor = (e) => {
+    hideEditor = !hideEditor;
+    if (hideEditor) {
+        editorContainer.style.width = "0%";
+        dbContainer.style.width = "100%";
+        hideEditorBtn.textContent = "EDIT";
+    } else {
+        editorContainer.style.width = "40%";
+        dbContainer.style.width = "60%";
+        hideEditorBtn.textContent = "PREVIEW";
+    }
+    editor.renderer.updateFull();
+};
+
 window.addEventListener("message", (e) => {
   if (e.data === "ready") {
     send();
   }
 });
-
+hideEditorBtn.onclick = hideShowEditor;
+updateBtn.onclick = (e) => send();
 editorEl.onkeydown = (e) => onkey(e);
 setInterval(() => {
-  if (currentContent !== editor.getValue()) {
-    currentContent = editor.getValue();
+  if (currentValue !== editor.getValue()) {
+    currentValue = editor.getValue();
     send();
   }
 }, 2000);
+
+const urlParams = new URLSearchParams(window.location.search);
+const importUrl = urlParams.get("import");
+const preview = urlParams.get("preview");
+
+if (importUrl) {
+  fetch(importUrl, (r) => r.text())
+    .then((data) => data.text().then((v) => editor.session.setValue(v)))
+    .catch((e) => editor.session.setValue(defaultYML));
+} else {
+  editor.session.setValue(defaultYML);
+}
+
+if (preview) {
+  hideShowEditor();
+}
